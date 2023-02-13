9550da574dc0aa3258069ff797a87b9f265e488e
ano201 <ahmu689@gmail.com>
2023-02-13 08:50:34 +0600

    footer started

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light py-3">
+      <div className="container">
+        <p className="text-center">Copyright &copy; Quote Gallery 2023</p>
+        <div className="d-flex justify-content-center">
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
