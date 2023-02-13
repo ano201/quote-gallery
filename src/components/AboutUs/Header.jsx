ae4d79e7691f27df3c638c62595817ede66504b0
ano201 <ahmu689@gmail.com>
2023-02-13 10:03:42 +0600

    dynamic router

--- src/components/AboutUs/Header.jsx
+++ src/components/AboutUs/Header.jsx
@@ -4,7 +4,7 @@ import "./AboutUs.css";
 const Header = () => {
   return (
     <div>
-      <div class="c-container">
+      <div className="c-container">
         <h1>About Quote Gallery</h1>
       </div>
     </div>
