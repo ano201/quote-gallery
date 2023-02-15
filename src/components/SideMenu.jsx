8f026043a3149f2b76bcb5c0db95f497c6ef7667
ano201 <ahmu689@gmail.com>
2023-02-15 15:01:54 +0600

    side menu started

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { Row } from "react-bootstrap";
+
+const SideMenu = () => {
+  const show = "col-sm-6 bg-danger";
+  return (
+    <Row>
+      <div className={show}>
+        <div className="bg-warning">
+          <h1>SideMenu</h1>
+        </div>
+        <div className="d-flex bg-primary justify-content-end">
+          <div className="w-25">
+            <FontAwesomeIcon icon={faCircleChevronLeft} className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </Row>
+  );
+};
+
+export default SideMenu;
