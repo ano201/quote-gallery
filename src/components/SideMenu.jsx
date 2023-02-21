e1e63808005457186beeb0687354551721f0947d
ano201 <ahmu689@gmail.com>
2023-02-21 15:25:19 +0600

    simple navigation added

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useState } from "react";
 import { Nav } from "react-bootstrap";
 import "./Sidemenu.css";
 import { Link } from "react-router-dom";
@@ -10,17 +10,39 @@ const SideMenu = () => {
     { title: "Home", href: "/" },
     { title: "About", href: "/about" },
   ];
+
+  const [flipIcon, setFlipIcon] = useState("horizontal");
+
+  const showNav = () => {
+    const nav = document.querySelector("#navigation");
+    nav.classList.toggle("hide");
+    nav.classList.contains("hide")
+      ? setFlipIcon("horizontal")
+      : setFlipIcon("");
+  };
+
   return (
-    <Nav className="flex-column nav-container">
-      <div>
-        <FontAwesomeIcon icon={faCircleChevronLeft} className="img-fluid" />
-      </div>
-      <div className="link-container">
+    <Nav
+      id="navigation"
+      className=" d-flex justify-content-between nav-container hide"
+    >
+      <div className="link-container d-flex flex-column align-items-center pt-5 bg-black">
         {links.map((link) => (
           <Nav.Item key={link.href}>
-            <Link to={link.href}>{link.title}</Link>
+            <Link className="nav-link" to={link.href}>
+              {link.title}
+            </Link>
           </Nav.Item>
         ))}
+      </div>
+      <div className="nav-button">
+        <FontAwesomeIcon
+          icon={faCircleChevronLeft}
+          onClick={showNav}
+          className="img-fluid mt-3 ms-1"
+          inverse
+          flip={flipIcon}
+        />
       </div>
     </Nav>
   );
